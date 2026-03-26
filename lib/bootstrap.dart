9b470ffea07dec538a4f1ff17a8dc27cb4c9797d
dduru26 <d.duru@alustudent.com>
2026-03-26 21:17:59 +0200

    fix: show startup state during Firebase init

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'src/core/di/providers.dart';
 import 'src/core/env/app_flavor.dart';
-import 'src/app/jrnl_app.dart';
+import 'src/app/bootstrap_app.dart';
 
 Future<void> bootstrap({required AppFlavor flavor}) async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
@@ -20,12 +15,5 @@ Future<void> bootstrap({required AppFlavor flavor}) async {
     ),
   );
 
-  runApp(
-    ProviderScope(
-      overrides: [
-        appFlavorProvider.overrideWithValue(flavor),
-      ],
-      child: const JrnlApp(),
-    ),
-  );
+  runApp(BootstrapApp(flavor: flavor));
 }
