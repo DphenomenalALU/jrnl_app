3cd00493def42f63c783f606f58ef224ba04b81d
ZeeyahOke <f.oke@alustudent.com>
2026-03-28 03:52:55 +0200

    chore: add mock data flag for production

--- lib/src/core/env/app_env.dart
+++ lib/src/core/env/app_env.dart
@@ -4,19 +4,32 @@ class AppEnv {
   const AppEnv({
     required this.flavor,
     required this.appName,
+    required this.useMockData,
   });
 
   final AppFlavor flavor;
   final String appName;
+  final bool useMockData;
 
   bool get isDev => flavor == AppFlavor.dev;
   bool get isProd => flavor == AppFlavor.prod;
 }
 
 AppEnv appEnvForFlavor(AppFlavor flavor) {
+  const useMockData = bool.fromEnvironment(
+    'USE_MOCK_DATA',
+    defaultValue: false,
+  );
   return switch (flavor) {
-    AppFlavor.dev => const AppEnv(flavor: AppFlavor.dev, appName: 'JRNL (Dev)'),
-    AppFlavor.prod => const AppEnv(flavor: AppFlavor.prod, appName: 'JRNL'),
+    AppFlavor.dev => const AppEnv(
+        flavor: AppFlavor.dev,
+        appName: 'JRNL (Dev)',
+        useMockData: useMockData,
+      ),
+    AppFlavor.prod => const AppEnv(
+        flavor: AppFlavor.prod,
+        appName: 'JRNL',
+        useMockData: false,
+      ),
   };
 }
-
